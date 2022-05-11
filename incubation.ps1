075b4aca89f57df55643ec1aa90c86fe423c2f83
RamanVashisth <95477205+RamanVashisth@users.noreply.github.com>
2022-05-11 19:30:01 +0530

    Delete incubation.ps1

--- incubation.ps1
+++
@@ -1,9 +0,0 @@
-connect-AzAccount
-$rg= 'incubation-RG'
-New-AzResourceGroup -Name $rg -Location 'eastus' -Force
-
-New-AzResourceGroupDeployment `
--ResourceGroupName $rg `
--TemplateFile 'incubation.json' `
--TemplateParameterFile 'incubation.parameters.json'
-
